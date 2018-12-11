9dc662c900d2c582d636ad9c9569c5bbd5adf5e2
Sara Kevorkian <sdkevorkian@gmail.com>
2018-12-11 14:36:25 -0800

    starting to add styled components to header & theme to page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite: '#EdEdEd',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0,0,0,0.09)',
+    breakpoint: '1300px'
+};
+
+theme.gray = theme.grey;
+theme.lightgray = theme.grey;
+
+const StyledPage = styled.div`
+    background: white;
+    color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+    max-width: ${props => props.theme.maxWidth};
+    margin: 0 auto;
+    padding: 2rem;    
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
-                <Meta />
-                <Header />                
-                {this.props.children}
-            </div>
+            <ThemeProvider theme={theme}>
+                <StyledPage>
+                    <Meta />
+                    <Header />     
+                    <Inner>
+                        {this.props.children}
+                    </Inner>       
+                </StyledPage>
+            </ThemeProvider>
         );
     }
 }
