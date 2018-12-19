2e5ce4386ff45865bb0e33a3bd6c7e22c0723607
Sara Kevorkian <sdkevorkian@gmail.com>
2018-12-18 16:02:55 -0800

    setting up prisma queries and mutations

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){
+        //TODO: check if logged in
+        const item = await ctx.db.mutation.createItem({
+            data:{
+                ...args //spread all object properties
+            }
+        }, info); //info passes query so database can return the item after creating
+
+        return item; //return item after promise resolved
+    }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {};
+const {forwardTo} = require('prisma-binding');
+
+const Query = {
+    //this is where database calls go regardless of db backend (which is prisma)
+    // async items(parent, args, ctx, info){
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+    //use below if going to be exactly like strict db call
+    //good to use when setting up, before needing to set up custom logic
+    items: forwardTo('db'),
+};
 
 module.exports = Query;
