a2ed544c1f287f1883be46f9e1fe8b1ef90457cf
Sara Kevorkian <sdkevorkian@gmail.com>
2018-12-19 15:09:53 -0800

    set up apollo client

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,4 +1,4 @@
-import withApollo from 'next-with-apollo';
+import withApollo from 'next-with-apollo';//set up with server side rendering (next.js)
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
@@ -6,6 +6,7 @@ function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     request: operation => {
+      //when you make a request, will take loggedIn cookies
       operation.setContext({
         fetchOptions: {
           credentials: 'include',
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,20 +1,35 @@
 import App, {Container} from 'next/app';
 import Page from '../components/Page';
+import {ApolloProvider} from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+    static async getInitialProps({Component, ctx}){
+        //nextJS lifecycle method
+        let pageProps = {};
+        //crawls pages, fetches data and returns data, used for server side applications
+        //comes from next.js & apollo docs examples
+        if(Component.getInitialProps){
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        //this exposes the query to the user
+        pageProps.query = ctx.query;
+        return {pageProps};
+    }
     render(){
-        const {Component} = this.props;
+        const {Component, apollo, pageProps} = this.props;
         //this is the component that holds each page
         return (
             <Container>
-                <Page>
-                    <Component />
-                </Page>
+                <ApolloProvider client={apollo}>
+                    <Page>
+                        <Component {...pageProps}/>
+                    </Page>
+                </ApolloProvider>
             </Container>
         )
     }
 }
 
-
-
-export default MyApp;
+//make client accessable with this.props.
+export default withData(MyApp);
