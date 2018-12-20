ad618927418c33bd03bd218561eac95a5dc288fa
Sara Kevorkian <sdkevorkian@gmail.com>
2018-12-19 16:15:34 -0800

    adding items with querys from client

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 //link will create links between pages without causing a full refresh
 //uses HTML5 push state
+import Items from '../components/Items';
 
 const Home = props => (
     <div>
-        <p>Hey!</p>
+        <Items/>
     </div>
 );
 
