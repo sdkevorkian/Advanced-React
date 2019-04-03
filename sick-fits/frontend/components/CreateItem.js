464d3260f1045df832cee2addbecb7fe2fd4d068
Sara Kevorkian <sara.kevorkian@avanade.com>
2019-04-02 17:30:50 -0700

    can upload images to cloudinary

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -28,16 +28,32 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
     state={
-        title: 'test',
-        description: ' fake data ',
-        image: 'dog.jpg',
-        largeImage: 'big-dog.jpg',
-        price: 100.00
+        title: '',
+        description: '',
+        image: '',
+        largeImage: '',
+        price: 0
     };
-    handleChange = (e ) => {
+    handleChange = (e) => {
         const {name, type, value} = e.target;
         const val = type === "number" ? parseFloat(value) : value;   
         this.setState({[name]: val});
+    }
+    uploadFile = async (e) =>{
+        console.log('uploading file...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+        const res = await fetch('https://api.cloudinary.com/v1_1/sdkevorkian/image/upload', {
+            method: 'POST',
+            body: data,
+        });
+        const file = await res.json();
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url,
+        });
     }
     render (){
         return (
@@ -52,7 +68,6 @@ class CreateItem extends Component {
                         //call the mutation
                         const res = await createItem();
                         //go to single item page
-                        console.log(res);
                         Router.push({
                             pathname: '/item',
                             query: {id: res.data.createItem.id}
@@ -62,6 +77,19 @@ class CreateItem extends Component {
                     <h2>Sell an item.</h2>
                     <Error error={error} />
                     <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="file">
+                            Image
+                            <input 
+                                type="file" 
+                                id="file" 
+                                name="file" 
+                                placeholder="Upload an image" 
+                                required 
+                                // value={this.state.image}
+                                onChange={this.uploadFile}
+                                />
+                                {this.state.image && <img src={this.state.image} alt="Uploaded Preview"/>}
+                        </label>
                         <label htmlFor="title">
                             Title
                             <input 
